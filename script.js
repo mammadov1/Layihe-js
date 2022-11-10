53c55173a23488833aabf4cb68f74bb6c7107d2b
unknown <memmedovabdulmecid12@gmail.com>
2022-11-11 01:55:57 +0300

    mecid

--- script.js
+++ script.js
@@ -1,206 +1,162 @@
-const buttontdl = document.querySelector(".buttons");
-const inputtdl = document.querySelector("#inputcol");
-const deleteI = document.querySelector(".photo1");
-const mainlist = document.querySelector(".main");
-const image = document.querySelector(".photo");
-const clonI = inputtdl.cloneNode(true);
+let addbutton = document.querySelector(".buttons")
+let inpNode = document.querySelector("#inputdiv")
 
+let sortimage = document.querySelector(".sortimage")
+let tasksbox = document.querySelector(".tasks")
 
-deleteI.addEventListener("click", () => {
-    document.getElementById('enter').value = ""
+let inpDel = document.querySelector(".removeinp");
+let inpClone = inpNode.cloneNode(true)
+
+inpDel.addEventListener("click",()=>{
+    document.getElementById("enter").value = ""
 })
-
-buttontdl.addEventListener("click", () => {
-    let test = document.querySelector("#inputcol");
-
-    if (test == null) {
-        const valueElement = document.querySelector(".main");
-        valueElement.append(clonI)
-        document.getElementById('enter').value = ""
-
-        const deleteI = document.querySelector(".photo1");
-        deleteI.addEventListener("click", () => {
-            document.getElementById('enter').value = ""
+addbutton.addEventListener("click",()=>{
+    let test = document.querySelector("#inputdiv")
+    if(test == null){
+        const appendElement = document.querySelector(".tasks");
+        appendElement.append(inpClone)
+        document.getElementById("enter").value = ""
+        let inpDel = document.querySelector(".removeinp");
+        inpDel.addEventListener("click",()=>{
+        document.getElementById("enter").value = ""
         })
     }
-
-    let number = 0;
-    document.querySelectorAll(".main").forEach(() => {
-
-        number++;
+    let count2i=0;
+    document.querySelectorAll(".task").forEach(()=>{
+        count2i++;
     })
-
-    if (number > 4) {
-        let valuee = document.querySelector(".main");
-        valuee.scrollTop = valuee.scrollHeight;
+    if(count2i>4){
+        let tasksel = document.querySelector(".tasks")
+        tasksel.scrollTop = tasksel.scrollHeight;
+        
     }
+    let  input = document.getElementById("enter")
+    input.addEventListener("keypress",(event)=>{
+    if(event.key === 'Enter'){
+        if( document.getElementById("enter").value != ""){
+            let task = input.value;
+            let node = document.getElementById("simple")
+            let clone = node.cloneNode(true)
+            clone.removeAttribute('id');
+            clone.classList.add("task")
 
-    let input = document.getElementById("enter");
-    input.addEventListener('keypress', (event) => {
+            const appendElement = document.querySelector(".tasks");
+            appendElement.append(clone)
 
-        if (event.key === 'Enter') {
-            if (document.getElementById('enter').value != "") {
-                let one = input.value;
-                let two = document.getElementById('.main2');
-                let three = two.cloneNode(true);
+            let tasksel = document.querySelector(".tasks")
+            tasksel.scrollTop = tasksel.scrollHeight;
 
-                three.removeAttribute('id');
-                three.classList.add("main")
-                const appendElement = document.querySelector('.main');
-                appendElement.append(three)
+            let newtasktext = document.querySelector(".task:last-child .taskin")
+            newtasktext.innerHTML = task
 
-                let valuee = document.querySelector('.main');
-                valuee.scrollTop = valuee.scrollHeight;
+            let removeinp = document.querySelector("#inputdiv")
+            appendElement.removeChild(removeinp)
 
-                let value2 = document.querySelector(".task:last-child .header")
-                value2.innerHTML = task
+            const remove = document.querySelector(".task:last-child .remove")
+            remove.addEventListener("click",()=>{
+                appendElement.removeChild(clone)
 
-                let removecol = document.querySelector("#inputcol");
-                appendElement.removeChild(removecol)
-
-                const remove = document.querySelector('.task:lastchild .photo1remove')
-                remove.addEventListener('click', () => {
-
-                    appendElement.removeChild(three)
-
-                    let numberelement = document.querySelectorAll(".main");
-                    let number2 = 0;
-
-                    numberelement.forEach((item) => {
-
-                        number2++;
-                    })
-
-                    if (number2 == 0) {
-
-                        const appendElement = document.querySelector(".main");
-                        appendElement.append(clonI);
-                        document.getElementById('enter').value = ""
-                    }
+                let countelement = document.querySelectorAll(".task")
+                let count=0;
+                countelement.forEach((item)=>{
+                    count++;
+                    
                 })
-
-                const newelement = document.querySelectorAll(".task");
-                let number3 = 0;
-
-                newelement.forEach(() => {
-
-                    number3++;
-                })
-            }
-        }
-    })
-})
-
-let input = document.getElementById("enter")
-input.addEventListener('keypress', (event) => {
-
-    if (event.key === 'Enter') {
-
-        if (document.getElementById("enter").value != "") {
-            let one = input.value;
-            let two = document.getElementById('main2');
-            let three = two.cloneNode(true)
-
-            three.removeAttribute('id');
-            three.classList.add('task');
-
-            const appendElement = document.querySelector(".main");
-            appendElement.append(three);
-
-            let value2 = document.querySelector(".task:last-child .header")
-            value2.innerHTML = task
-
-            let removecol = document.querySelector("#inputcol");
-            appendElement.removeChild(removecol)
-
-            const remove = document.querySelector('.task:lastchild .photo1remove')
-            remove.addEventListener('click', () => {
-
-                appendElement.removeChild(three);
-
-                let numberelement = document.querySelectorAll(".task");
-                let number2 = 0;
-
-                numberelement.forEach(() => {
-
-                    number2++;
-                })
-
-                if (number2 == 0) {
-
-                    const appendElement = document.querySelector(".main");
-                    appendElement.append(clonI);
-                    document.getElementById('enter').value = ""
-
-
-                    let deleteI = document.querySelector(".photo1");
-                    deleteI.addEventListener('click', () => {
-                        document.getElementById('enter').value = ""
-                    })
-
+                if(count == 0){
+                    const appendElement = document.querySelector(".tasks");
+                    appendElement.append(inpClone)
+                    document.getElementById("enter").value = ""
                 }
-
             })
-
+            const taskelements = document.querySelectorAll(".task")
+            let countsc=0;
+            taskelements.forEach(()=>{
+                countsc++;
+            })
         }
-
     }
 })
-function sortListDir() {
+})
 
-    let i, switching, b, shouldSwitch, dir, switchcount = 0;
-
-    switching = true;
-    dir = "asc";
-
-    while (switching) {
-
-        switching = false;
-        b = mainlist.querySelectorAll('.task');
-        for (i = 0; i < b.length - 1; i++) {
-            shouldSwitch = false;
-
-            if (dir == "asc") {
-
-                if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-
-                    shouldSwitch = true;
-
-                    sortimage.src = "./picture/Group 73 (1).png";
-                    break;
-                }
-            }
-            else if (dir == "desc") {
-
-                if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-
-                    shouldSwitch = true;
-
-                    sortimage.src = "./picture/Group 91.png";
-                    break;
-                }
-            }
+function sortArray() {
+  var i, switching, b, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  dir = "asc";
+  
+  while (switching) {
+    
+    switching = false;
+    b = tasksbox.querySelectorAll(".task");
+  
+    for (i = 0; i < b.length - 1; i++) {
+      shouldSwitch = false;
+      if (dir == "asc") {
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          sortimage.src = "/image/arrow.svg";
+          break;
         }
-
-        if (shouldSwitch) {
-
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
-
-            switching = true;
-
-            switchcount++;
+      } else if (dir == "desc") {
+        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          sortimage.src = "/image/deletearrow.svg";
+          break;
         }
-        else {
-
-            if (switchcount == 0 && dir == "asc") {
-
-                dir = "desc";
-                switching = true;
-            }
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+let  number = document.getElementById("enter")
+    number.addEventListener("keypress",(event)=>{
+    if(event.key === 'Enter'){
+        
+        if( document.getElementById("enter").value != ""){
+            let task = number.value;
+            let node = document.getElementById("simple")
+            let clone = node.cloneNode(true)
+            clone.removeAttribute('id');
+            clone.classList.add("task")
+            const appendElement = document.querySelector(".tasks");
+            appendElement.append(clone)
+            
+            let newtasktext = document.querySelector(".task:last-child .taskin")
+            newtasktext.innerHTML = task
+            let removeinp = document.querySelector("#inputdiv")
+            appendElement.removeChild(removeinp)
+            
+            const remove = document.querySelector(".task:last-child .remove")
+            remove.addEventListener("click",()=>{
+                appendElement.removeChild(clone)
+                let countelement = document.querySelectorAll(".task")
+                let count=0;
+                countelement.forEach(()=>{
+                    count++;
+                })
+                if(count == 0){
+                    const appendElement = document.querySelector(".tasks");
+                    appendElement.append(inpClone)
+                    document.getElementById("enter").value = ""
+                    let inpDel = document.querySelector(".removeinp");
+                    inpDel.addEventListener("click",()=>{
+                    document.getElementById("enter").value = ""
+                    })
+                }
+            })
+            
         }
     }
-}
+})
 
-new Sortable(mainlist, {
-    animation: 200,
+new Sortable(tasksbox, {
+animation: 200,
 });
